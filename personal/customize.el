0f08ef6218e7af1aa3d30a2b15f6e73619a630d7
hanks <zhouhan315@gmail.com>
2015-06-21 11:33:49 +0900

    add font set in customize.el

--- personal/customize.el
+++ personal/customize.el
@@ -99,4 +99,15 @@
 (scroll-down arg)
 (scroll-down 3)))
 
+;; set window size
+(setq default-frame-alist
+'((height . 35) (width . 135)))
+
+;; set the current frame background and font.
+(set-frame-font "-apple-Monaco-medium-normal-normal-*-16-*-*-*-m-0-iso10646-1" nil t)
+
+;; set the font and background for all other frames.
+(add-to-list 'default-frame-alist
+             '(font .  "-apple-Monaco-medium-normal-normal-*-16-*-*-*-m-0-iso10646-1"))
+
 ;;; customize.el ends here
