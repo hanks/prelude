53cc0609d5075e9bc83a694de01cf0595e5f44a4
hanks <zhouhan315@gmail.com>
2015-07-02 07:16:30 +0900

    hide unnessary tab

--- personal/customize.el
+++ personal/customize.el
@@ -69,6 +69,30 @@
  '(tabbar-separator ((t (:inherit tabbar-default :background "#95CA59"))))
  '(tabbar-unselected ((t (:inherit tabbar-default)))))
 
+(defvar my-tabbar-displayed-buffers
+ '("scratch*" "*Messages*" "*Backtrace*" "*Colors*" "*Faces*")
+  "*Regexps matches buffer names always included tabs.")
+(defun my-tabbar-buffer-list ()
+  "Return the list of buffers to show in tabs.
+Exclude buffers whose name starts with a space or an asterisk.
+The current buffer and buffers matches `my-tabbar-displayed-buffers'
+are always included."
+  (let* ((hides (list ?\  ?\*))
+         (re (regexp-opt my-tabbar-displayed-buffers))
+         (cur-buf (current-buffer))
+         (tabs (delq nil
+                     (mapcar (lambda (buf)
+                               (let ((name (buffer-name buf)))
+                                 (when (or (string-match re name)
+                                           (not (memq (aref name 0) hides)))
+                                   buf)))
+                             (buffer-list)))))
+    ;; Always include the current buffer.
+    (if (memq cur-buf tabs)
+        tabs
+      (cons cur-buf tabs))))
+(setq tabbar-buffer-list-function 'my-tabbar-buffer-list)
+
 ;; set smart cursor
 (require 'cursor-change)
 (cursor-change-mode)
